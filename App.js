c4aa9c69093cd9da4bc30802bab433b20c883e53
Ishimwe William <ishimwe.william2000@gmail.com>
2025-05-06 11:50:51 +0200

    forth Chapter -b

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native"
+// import {createDrawerNavigator} from "@react-navigation/drawer";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
+import UserScreen from "./src/screens/UserScreen";
+import {Ionicons} from "@expo/vector-icons"
+import {StatusBar} from "expo-status-bar";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+
+const BottomTabNavigator = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+        <>
+            {/*<StatusBar style={"light"}/>*/}
+            <NavigationContainer>
+                <BottomTabNavigator.Navigator screenOptions={{
+                    headerStyle: {backgroundColor: "#152544"},
+                    headerTintColor: 'white',
+                    tabBarActiveTintColor: '#152544',
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+                }}>
+                    <BottomTabNavigator.Screen
+                        name={"Welcome"}
+                        component={WelcomeScreen}
+                        options={{
+                            tabBarIcon: ({color, size}) => <Ionicons name={"home"} size={size} color={color}/>
+                        }}
+                    />
+                    <BottomTabNavigator.Screen
+                        name={"User"}
+                        component={UserScreen}
+                        options={{
+                            tabBarIcon: ({color, size}) => <Ionicons name={"person"} size={size} color={color}/>
+                        }}
+                    />
+                </BottomTabNavigator.Navigator>
+            </NavigationContainer>
+        </>
+    )
+}
